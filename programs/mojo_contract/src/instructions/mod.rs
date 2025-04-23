4367eb6b0c12be376e3dccfebb21f035aa6ab865
qhristen <odeyxtian@gmail.com>
2025-04-23 11:35:50 +0100

    chore: update dependencies and add new instructions
    - Updated @coral-xyz/anchor from version 0.30.1 to 0.31.1 in package.json and yarn.lock.
    - Updated anchor-lang from version 0.30.1 to 0.31.1 in Cargo.toml.
    - Added new instruction files: collect_fees.rs, pause_platform.rs, and update_fee_rate.rs.
    - Updated mod.rs to include new instruction modules.

--- programs/mojo_contract/src/instructions/mod.rs
+++ programs/mojo_contract/src/instructions/mod.rs
@@ -3,9 +3,15 @@ pub mod add_liquidity;
 pub mod create_pair;
 pub mod remove_liquidity;
 pub mod swap;
+pub mod collect_fees;
+pub mod pause_plaform;
+pub mod update_fee_rate;
 
 pub use initialize_platform::*;
 pub use add_liquidity::*;
 pub use create_pair::*;
 pub use remove_liquidity::*;
 pub use swap::*;
+pub use collect_fees::*;
+pub use pause_plaform::*;
+pub use update_fee_rate::*;
